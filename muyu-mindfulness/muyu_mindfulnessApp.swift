fdb24bdbc18277dc245910c65a53fc8fcf74f03c
yuanjun <eugenelau@foxmail.com>
2024-08-25 10:36:35 +0800

    first create muyu-mindfulness

---
+++ muyu-mindfulness/muyu_mindfulnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  muyu_mindfulnessApp.swift
+//  muyu-mindfulness
+//
+//  Created by yuanjun on 2024/8/25.
+//
+
+import SwiftUI
+
+@main
+struct muyu_mindfulnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
