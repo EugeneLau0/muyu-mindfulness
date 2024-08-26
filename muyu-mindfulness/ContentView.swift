6ff12726939ffec521cc442f5cb4cf228a80e566
yuanjun <eugenelau@foxmail.com>
2024-08-26 11:51:11 +0800

    create sound

--- muyu-mindfulness/ContentView.swift
+++ muyu-mindfulness/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
         VStack {
             // 显示计数器的标签
             Text("计数: \(counter)").font(.largeTitle)
-            
+            Spacer()
             // 创建一个按钮，点击时调用incrementCounter函数
             Button(action: {
                 // 每次点击，计数器增加1
@@ -45,7 +45,21 @@ struct ContentView: View {
         }.onDisappear {
             audioPlayer?.stop()
             audioPlayer = nil
-        }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            // 添加一个透明的覆盖层来检测点击
+            GeometryReader { geometryProxy in
+                Color.clear
+                    .opacity(0) // 设置透明度为0，使其不影响视觉效果
+                    .edgesIgnoringSafeArea(.all) // 确保覆盖包括安全区域在内的整个屏幕
+                    .onTapGesture {
+                        // 整个屏幕的点击事件处理
+                        print("屏幕被点击")
+                        self.incrementCounter()
+                        self.playSound()
+                    }
+            }
+        )
     }
     
     // 定义一个函数，用于增加计数器的值
@@ -56,7 +70,7 @@ struct ContentView: View {
     private func playSound() {
         audioPlayer?.play()
     }
-    
+    // 音频准备
     private func prepareSound() {
         let sourceName = "sound_2"
         let extensionName = "mp3"
@@ -67,6 +81,7 @@ struct ContentView: View {
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
             audioPlayer?.prepareToPlay()
+            print("将使用音频文件：\(sourceName).\(extensionName)")
         } catch {
             print("无法创建音频播放器: \(error.localizedDescription)")
         }
